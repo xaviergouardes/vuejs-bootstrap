1d1e0d6d3c82185d54164f674d0500d913982867
Xavier Gouardes <x.gouardes@deal.fr>
2019-06-25 10:17:42 +0200

    ajout d'un icon d'application dans le header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,17 @@
 <template>
 
   <nav class="navbar navbar-light bg-light navbar-expand-sm ">
-    <a class="navbar-brand" href="#">Navbar</a>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link class="nav-link" active-class="active" to="/articles" >Accueil</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" active-class="active" to="/sports" >Sports</router-link>
-        </li>
-      </ul>
+    <a class="navbar-brand" href="#">
+      <img src="/icons8-puzzle.svg">
+    </a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link class="nav-link" active-class="active" to="/articles" >Accueil</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" active-class="active" to="/sports" >Sports</router-link>
+      </li>
+    </ul>
   </nav>
 
 </template>
