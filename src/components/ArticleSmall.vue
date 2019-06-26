928542c9e5df98401bde709cc46ef15a461fc602
Xavier Gouardes <x.gouardes@deal.fr>
2019-06-26 14:14:32 +0200

    --amend

--- src/components/ArticleSmall.vue
+++ src/components/ArticleSmall.vue
@@ -8,7 +8,7 @@
       <h5 class="card-title"> {{ article.text }}</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      <router-link :to="{ path: '/article/' + this.article.id}" class="card-link">En savoir plus ...</router-link>
+      <router-link :to="{ path: '/article/' + this.article.id }" class="card-link">En savoir plus ...</router-link>
   </div>
 
   <div class="card-footer text-muted">
